e971dcac324eb02cd19c7f9147f40c20042fd871
minervadexab <152941258+minervadexab@users.noreply.github.com>
2026-05-30 09:59:49 +0700

    Test Push

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,7 +33,7 @@
 <div class="mt-10 bg-white p-6 rounded-2xl shadow">
 
     <h2 class="text-2xl font-semibold mb-4">
-        Welcome Back 👋
+        Welcome Back BROWWWW
     </h2>
 
     <p class="text-gray-600">
